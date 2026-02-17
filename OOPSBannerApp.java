759d7ba0a7558893415deb3c9cd79dea0ed2dc31
vedansh3805-gif <Vedansh.3805@gmail.com>
2026-02-17 09:39:46 +0530

    UC1: Print literal OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print literal text "OOPS"
+ * @author Vedansh
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
