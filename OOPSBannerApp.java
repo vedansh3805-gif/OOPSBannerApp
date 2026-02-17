f76ce1f02cc70796a30c475ac2f884f58b2b81d1
vedansh3805-gif <Vedansh.3805@gmail.com>
2026-02-17 11:11:00 +0530

    UC3: Refactor banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,39 @@
 /**
  * OOPSBannerApp
- * UC2: Print large banner "OOPS" using * and spaces
+ * UC3: Display OOPS banner using String.join()
  * @author Vedansh
- * @version 2.0
+ * @version 3.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    *****    ***** ");
-        System.out.println("*     *  *     *  *     *  *     *");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  *****    *****  ");
-        System.out.println("*     *  *     *  *            *  ");
-        System.out.println("*     *  *     *  *            *  ");
-        System.out.println(" *****    *****    *      *****   ");
+        System.out.println(String.join("",
+                " *****    *****    *****    ***** "
+        ));
 
+        System.out.println(String.join("",
+                "*     *  *     *  *     *  *     *"
+        ));
+
+        System.out.println(String.join("",
+                "*     *  *     *  *     *  *      "
+        ));
+
+        System.out.println(String.join("",
+                "*     *  *     *  *****    *****  "
+        ));
+
+        System.out.println(String.join("",
+                "*     *  *     *  *            *  "
+        ));
+
+        System.out.println(String.join("",
+                "*     *  *     *  *            *  "
+        ));
+
+        System.out.println(String.join("",
+                " *****    *****    *      *****   "
+        ));
     }
 }
