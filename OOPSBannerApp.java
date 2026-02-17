c4db7ba9427c503bc827ec99e4a34f5ac1b8da9c
vedansh3805-gif <Vedansh.3805@gmail.com>
2026-02-17 10:16:30 +0530

    UC2: Display OOPS banner using ASCII art

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,20 @@
 /**
  * OOPSBannerApp
- * UC1: Print literal text "OOPS"
+ * UC2: Print large banner "OOPS" using * and spaces
  * @author Vedansh
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *****    *****  ");
+        System.out.println("*     *  *     *  *            *  ");
+        System.out.println("*     *  *     *  *            *  ");
+        System.out.println(" *****    *****    *      *****   ");
+
     }
 }
