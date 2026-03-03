65238102621e12525e76b4dcac670869d4c6a607
vedansh3805-gif <Vedansh.3805@gmail.com>
2026-03-03 09:58:18 +0530

    UC6: Implemented static helper methods for O, P, S banner generation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,31 +1,58 @@
-// UC5 - OOPS Banner App
-// Using Array Initialization with String.join() and Enhanced For Loop
+// UC6 - OOPS Banner App using Static Helper Methods
 
 public class OOPSBannerApp {
 
+    // Method to generate O pattern
+    public static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Method to generate P pattern
+    public static String[] getP() {
+        return new String[]{
+                "*****  ",
+                "*     *",
+                "*     *",
+                "*****  ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Method to generate S pattern
+    public static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        };
+    }
+
     public static void main(String[] args) {
 
-        // Declare and initialize array in a single statement
-        String[] bannerLines = {
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
 
-                String.join("", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*",
-                                   "*", "*", "*", "*", "*", "*", "*", "*", "*", "*"),
-
-                String.join("", "*", "                                      ", "*"),
-
-                String.join("", "*", "        WELCOME TO OOPS APP            ", "*"),
-
-                String.join("", "*", "        OBJECT ORIENTED PROGRAM        ", "*"),
-
-                String.join("", "*", "                                      ", "*"),
-
-                String.join("", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*",
-                                   "*", "*", "*", "*", "*", "*", "*", "*", "*", "*")
-        };
-
-        // Enhanced for loop to print banner
-        for (String line : bannerLines) {
-            System.out.println(line);
+        // Loop to print OOPS
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "  " +
+                               O[i] + "  " +
+                               P[i] + "  " +
+                               S[i]);
         }
     }
 }
